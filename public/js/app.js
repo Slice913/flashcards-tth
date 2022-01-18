1a748b7c356826c4b44f1a5d648334174beeb8db
Slice <akeemayoade@gmail.com>
2022-01-18 16:33:02 -0600

    show hint button fuctionailty working

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+const showHint = document.getElementById('show-hint');
+const hint = document.getElementById('hint');
+
+showHint.addEventListener('click', () => {
+    showHint.style.display = 'none';
+    hint.style.display = 'block';
+});
+
+// complete the on click function to the the hint when clicked
